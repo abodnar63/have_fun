21f8bf375361a158312006d8da734a5fbb04279c
Anton Bodnar <abodnar@inflection.com>
2015-05-30 21:50:08 +0300

    added usage of ng-model for cells behavior

--- app/assets/javascripts/crossword/main.js
+++ app/assets/javascripts/crossword/main.js
@@ -1,7 +1,20 @@
 var app = angular.module('crossword', []);
 
 app.controller('CrosswordController', function($scope) {
+	var CELL_STATES = {
+		active: "active",
+		inactive: "inactive;"
+	}
+
 	$scope.grid = {columns: 30, rows: 30}
+	$scope.grid.cells = [];
+	for (var i=0; i < $scope.grid.rows; i++) {
+		$scope.grid.cells[i] = [];
+
+		for (var j=0; j < $scope.grid.columns; j++) {
+			$scope.grid.cells[i].push({state: CELL_STATES.inactive});
+		}
+	}
 
 	$scope.getNumber = function(value) {
 		return new Array(value)
@@ -11,7 +24,11 @@ app.controller('CrosswordController', function($scope) {
 		return ($(".crossword-row").width() / $scope.grid.columns) + "px";
 	}
 
-	$scope.cellClick = function(event) {
-		$(event.target).toggleClass("active");
+	$scope.cellClick = function(cell) {
+		if (cell.state == CELL_STATES.active) {
+			cell.state = CELL_STATES.inactive;
+		} else {
+			cell.state = CELL_STATES.active;
+		}
 	}
 });
